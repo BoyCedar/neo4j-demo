02e86fea66c17c7f323275908c1e0427595e877b
qixs <qixs_gr@163.com>
2021-07-26 22:10:33 +0800

    修改查询

--- java-neo4j-server/src/main/java/com/study/ServerNeo4jShortPathQuery.java
+++ java-neo4j-server/src/main/java/com/study/ServerNeo4jShortPathQuery.java
@@ -13,10 +13,12 @@ import static org.neo4j.driver.Values.parameters;
 public class ServerNeo4jShortPathQuery {
 
     public static void main(String[] args) {
-        Driver driver = GraphDatabase.driver("neo4j://81.70.202.213:7687", AuthTokens.basic("neo4j","123456"));
+        Driver driver = GraphDatabase.driver("bolt://81.70.202.213:7687", AuthTokens.basic("neo4j","123456"));
         Session session = driver.session();
         String cql = "match p = shortestpath((n1:Person{name:$startName}) - [*] - (n2:Person{name:$endName})) return p";
-        Result result = session.run(cql,parameters("startName","王启年","endName","九品射手燕小乙"));
+        String cqlHaveScope = "match p = shortestpath((n1:Person{name:$startName}) - [*1..4] - (n2:Person{name:$endName})) return p";
+        String cqlMore = "match p = (n1:Person{name:$startName}) - [*] - (n2:Person{name:$endName}) return p";
+        Result result = session.run(cqlMore,parameters("startName","王启年","endName","九品射手燕小乙"));
         while (result.hasNext()) {
             Record record = result.next();
             System.out.println(record);
